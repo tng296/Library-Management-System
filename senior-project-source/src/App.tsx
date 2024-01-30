fa39e7efff3a7786f325857a81b47709742a4ae0
Vincent Nguyen <vincent.nguyen296@gmail.com>
2024-01-30 13:51:58 -0600

    added boostrap and sass

--- senior-project-source/src/App.tsx
+++ senior-project-source/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './App.scss'
 import AdminLoginPage from '/Users/vincentnguyen/Developer/Senior-Project/senior-project-source/senior-project-fe/pages/AdminLoginPage.tsx'
 import AdminRegisterPage from '/Users/vincentnguyen/Developer/Senior-Project/senior-project-source/senior-project-fe/pages/AdminRegisterPage.tsx'
 
 function App() {
   return (
     <div>
-      <AdminRegisterPage />
+      <h1>Hello World</h1>
     </div>
   )
 }
